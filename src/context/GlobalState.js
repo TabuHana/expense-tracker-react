bfd7b87602e1b149dd69ebe779a2adf8a56bf5ba
Tabu Hana <taboodog@hotmail.com>
2022-08-24 11:53:07 -0700

    frontend complete

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,9 @@
 import { createContext, useReducer } from "react";
-import AppReducer from './AppReducer'
+import AppReducer from './AppReducer';
 
 //Initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 },
-  ]
+  transactions: []
 };
 
 //Create context
@@ -18,8 +13,25 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //Actions
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  };
+
   return (<GlobalContext.Provider value={ {
-    transactions: state.transactions
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
   } }>
     { children }
   </GlobalContext.Provider>);
